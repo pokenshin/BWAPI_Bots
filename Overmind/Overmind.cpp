41cbd6e669934ff95a939ccbfff7767130c22e09
Jonas Augusto <pokenshin@hotmail.com>
2017-02-03 20:31:55 -0200

    v0.02 - Reorganized ExampleAiModule's code into functions

--- Overmind/Overmind.cpp
+++ Overmind/Overmind.cpp
@@ -4,17 +4,93 @@
 using namespace BWAPI;
 using namespace Filter;
 
+// Custom Functions
+// Error displaying function
+void displayError(Position pos, Error lastErr)
+{
+	// However, drawing the error once will only appear for a single frame
+	// so create an event that keeps it on the screen for some frames
+	Broodwar->registerEvent([pos, lastErr](Game*){ Broodwar->drawTextMap(pos, "%c%s", Text::White, lastErr.c_str()); },   // action
+		nullptr,    // condition
+		Broodwar->getLatencyFrames());  // frames to run
+}
+
+// Updates the on screen info
+void updateOnScreenInfo()
+{
+	// Display the game frame rate as text in the upper left area of the screen
+	Broodwar->drawTextScreen(200, 0, "FPS: %d", Broodwar->getFPS());
+	Broodwar->drawTextScreen(200, 20, "Average FPS: %f", Broodwar->getAverageFPS());
+}
+
+// Handles the Worker AI
+void handleWorkerAI(Unit u)
+{
+	// if our worker is idle
+	if (u->isIdle())
+	{
+		// Order workers carrying a resource to return them to the center,
+		// otherwise find a mineral patch to harvest.
+		if (u->isCarryingGas() || u->isCarryingMinerals())
+		{
+			u->returnCargo();
+		}
+		else if (!u->getPowerUp())  // The worker cannot harvest anything if it
+		{                             // is carrying a powerup such as a flag
+			// Harvest from the nearest mineral patch or gas refinery
+			if (!u->gather(u->getClosestUnit(IsMineralField || IsRefinery)))
+			{
+				// If the call fails, then print the last error message
+				Broodwar << Broodwar->getLastError() << std::endl;
+			}
+
+		} // closure: has no powerup
+	} // closure: if idle
+}
+
+void handleHatcheryAI(Unit u)
+{
+	// Orders the Hatchery to build more Drones.
+	if (u->isIdle() && !u->train(u->getType().getRace().getWorker()))
+	{
+		Position pos = u->getPosition();
+		Error lastErr = Broodwar->getLastError();
+		// If that fails, draw the error at the location so that you can visibly see what went wrong!
+		displayError(pos, lastErr);
+
+		// Retrieve the supply provider type in the case that we have run out of supplies
+		UnitType overlord = u->getType().getRace().getSupplyProvider();
+		static int lastChecked = 0;
+
+		// If we are supply blocked and haven't tried constructing more recently
+		if (lastErr == Errors::Insufficient_Supply &&
+			lastChecked + 400 < Broodwar->getFrameCount() &&
+			Broodwar->self()->incompleteUnitCount(overlord) == 0)
+		{
+			lastChecked = Broodwar->getFrameCount();
+
+			// Retrieve a unit that is capable of constructing the supply needed
+			Unit larva = u->getClosestUnit(GetType == overlord.whatBuilds().first &&
+				IsIdle && IsOwned);
+			// If the larva was found
+			if (larva)
+				// Morph the Overlord!
+				larva->train(overlord);
+		} // closure: insufficient supply
+	} // closure: failed to train idle unit
+}
+
+
+
+// End Custom Functions
+
+// Start BWAI Functions
 void Overmind::onStart()
 {
 	Broodwar->sendText("Awaken my child and embrace the glory that is your birthright.");
 }
 
 void Overmind::onEnd(bool isWinner)
-{
-
-}
-
-void Overmind::onFrame()
 {
 
 }
@@ -88,3 +164,58 @@ void Overmind::onUnitComplete(BWAPI::Unit unit)
 {
 
 }
+
+void Overmind::onFrame()
+{
+	// Update on screen information
+	updateOnScreenInfo();
+	
+	// Return if the game is a replay or is paused
+	if (Broodwar->isReplay() || Broodwar->isPaused() || !Broodwar->self())
+		return;
+
+	// Prevent spamming by only running our onFrame once every number of latency frames.
+	// Latency frames are the number of frames before commands are processed.
+	if (Broodwar->getFrameCount() % Broodwar->getLatencyFrames() != 0)
+		return;
+
+	// Iterate through all the units that we own
+	for (auto &u : Broodwar->self()->getUnits())
+	{
+		// Ignore the unit if it no longer exists
+		// Make sure to include this block when handling any Unit pointer!
+		if (!u->exists())
+			continue;
+
+		// Ignore the unit if it has one of the following status ailments
+		if (u->isLockedDown() || u->isMaelstrommed() || u->isStasised())
+			continue;
+
+		// Ignore the unit if it is in one of the following states
+		if (u->isLoaded() || !u->isPowered() || u->isStuck())
+			continue;
+
+		// Ignore the unit if it is incomplete or busy constructing
+		if (!u->isCompleted() || u->isConstructing())
+			continue;
+
+
+		// Finally make the unit do some stuff!
+
+
+		// If the unit is a worker unit
+		if (u->getType().isWorker())
+		{
+			// Calls the function to handle Worker-related stuff
+			handleWorkerAI(u);
+		}
+
+		// If the unit is a Hatchery
+		else if (u->getType().isResourceDepot())
+		{
+			// Calls the function to handle Hatchery AI
+			handleHatcheryAI(u);
+		}
+	} // closure: unit iterator
+}
+
--- Overmind/Overmind.h
+++ Overmind/Overmind.h
@@ -4,12 +4,12 @@
 class Overmind : public BWAPI::AIModule
 {
 public:
-	virtual void onStart(); //Chamado somente uma vez no começo do jogo.
-	virtual void onEnd(bool isWinner); //Chamado somente uma vez no fim do jogo. isWinner determina se o bot ganhou. Retornará falso se for um replay.
-	virtual void onFrame(); //Chamado uma vez a cada frame lógico no Broodwar
-	virtual void onSendText(std::string text); //Chamado quando o usuário tenta mandar uma mensagem de texto. Usado principalmente para debung ingame via comandos de texto.
-	virtual void onReceiveText(BWAPI::Player player, std::string text); //Chamado quando o cliente recebe uma mensagem de outro player. Pode ser usado para co-op ou chat.
-	virtual void onPlayerLeft(BWAPI::Player player); //Chamado quando um jogador deixa a partida
+	virtual void onStart(); //Called once at the start of the game.
+	virtual void onEnd(bool isWinner); //Called once when the game is over. isWinner returns true if the bot won. It will always return false if the game is a replay.
+	virtual void onFrame(); //Called once every logical frame in Brood War.
+	virtual void onSendText(std::string text); //Called when the (human) user sends a text message. Useful for debugging ingame and sending specific commands.
+	virtual void onReceiveText(BWAPI::Player player, std::string text); //Called when the bot recieves a message ingame. Useful for co-op orders/strategies.
+	virtual void onPlayerLeft(BWAPI::Player player); //Called when a player leaves the game
 	virtual void onNukeDetect(BWAPI::Position target); //Chamado quando "Nuclear Launch Detected". O target é onde o ponto de impacto foi marcado. Se não for um lugar visível do mapa, o target vai receber Positions::Unknown
 	virtual void onUnitDiscover(BWAPI::Unit unit); //Chamado quando determinada unidade é descoberta. Inclui o status do Flag::CompleteMapInformation
 	virtual void onUnitEvade(BWAPI::Unit unit); //Chamado quando determinada unidade sai da visão. Inclui o status do Flag::CompleteMapInformation
